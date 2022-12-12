a431675701881d0f4a01cfc6d2ae21eb0e00438e
Basavaraj <Basavaraj@LAPTOP-A630U333>
2022-12-12 18:14:21 +0530

    new project

---
+++ BasavarajELF/src/test/java/testcase/url2_Test.java
@@ -0,0 +1,24 @@
+package testcase;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class url2_Test {
+
+		public WebDriver driver;
+		 
+		@Test
+		public void honda() {
+			System.setProperty("webdriver.chrome.driver","./Drivers/chromedriver.exe");
+			driver=new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(25,TimeUnit.SECONDS);
+			driver.navigate().to("https://www.honda2wheelersindia.com/");
+			driver.close();
+			}
+		}
+
+
